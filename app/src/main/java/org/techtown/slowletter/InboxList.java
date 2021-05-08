c3203c4451fa326e9840b9bd46016054e746154d
Ka Eul <kaeul991020@gmail.com>
2021-05-09 00:49:43 +0900

    inbox basic setting

---
+++ app/src/main/java/org/techtown/slowletter/InboxList.java
@@ -0,0 +1,13 @@
+package org.techtown.slowletter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InboxList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inboxlist);
+    }
+}
---
+++ app/src/main/java/org/techtown/slowletter/InboxView.java
@@ -0,0 +1,14 @@
+package org.techtown.slowletter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InboxView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inboxview);
+    }
+}
