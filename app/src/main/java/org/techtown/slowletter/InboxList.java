a4544980c2360ab7b2375c2b424be3e6dbad128d
Ka Eul <kaeul991020@gmail.com>
2021-05-09 15:34:19 +0900

    implement backbutton

--- app/src/main/java/org/techtown/slowletter/InboxList.java
+++ app/src/main/java/org/techtown/slowletter/InboxList.java
@@ -1,6 +1,10 @@
 package org.techtown.slowletter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,14 @@ public class InboxList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inboxlist);
+
+        ImageButton inbox = (ImageButton) findViewById(R.id.BackButton1);
+        inbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
