2ba29fd00a88daf028e2378349a4d785f1866f7b
Ka Eul <kaeul991020@gmail.com>
2021-05-09 15:10:26 +0900

    mainactivity Screen change

--- app/src/main/java/org/techtown/slowletter/MainActivity.java
+++ app/src/main/java/org/techtown/slowletter/MainActivity.java
@@ -2,7 +2,10 @@ package org.techtown.slowletter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView inbox = (ImageView)findViewById(R.id.inbox_button);
+        inbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),InboxList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
